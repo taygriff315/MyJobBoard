3d276783288ee962ee4b3249cdb83ca972dbdc95
Taylor Griffin <titleist315@gmail.com>
2021-06-07 12:25:56 -0500

    -added gitignore -added IdentitySamples -added connections.config

--- MyJobBoard.UI.MVC/Startup.cs
+++ MyJobBoard.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(MyJobBoard.UI.MVC.Startup))]
 namespace MyJobBoard.UI.MVC
 {
     public partial class Startup
